5dea543bb1cba57f5f50378150b9e1193e2c4bd3
Stéphan Sainléger <contact@stephansainleger.fr>
2024-12-17 16:52:06 +0100

    cln: remove Elabore migration specific sql scripts

--- 16.0/pre_upgrade.sh
+++ 16.0/pre_upgrade.sh
@@ -6,20 +6,7 @@ echo "Prepare migration to 16.0..."
 copy_database ou15 ou16 ou16 || exit 1
 
 # Execute SQL pre-migration commands
-PRE_MIGRATE_SQL=$(cat <<'EOF'
-/* Remove duplicate entries in model utm.source */
-DELETE FROM utm_source
-WHERE id IN (
-    SELECT id
-    FROM (
-        SELECT id,
-               ROW_NUMBER() OVER (PARTITION BY name ORDER BY id) as row_num
-        FROM utm_source
-    ) t
-    WHERE t.row_num > 1
-);
-EOF
-	       )
+PRE_MIGRATE_SQL=""
 echo "SQL command = $PRE_MIGRATE_SQL"
 query_postgres_container "$PRE_MIGRATE_SQL" ou16 || exit 1
 
